fbdb692ec5ccadc6b00e45802d324755407de66b
Joshua Longbottom <joshual@WELL.OX.AC.UK>
2016-11-18 19:19:40 +0000

    Combine rasters, and generate a surface of snake species richness
    Script read in each of the snake species rasters, stacks them, then sums
    the values for each pixel across all layers in the stack. The resulting
    surface is a count of the number of venomous snake species within each
    pixel

---
+++ gen_species_richness.R
@@ -0,0 +1,52 @@
+# clear workspace
+rm(list = ls())
+
+# load required packages
+require(raster)
+
+# list all rasters in the directory
+list <- as.list(list.files("Z:/users/joshua/Snakebite/WHO EOR rasters/", 
+                           pattern = '*tif$',
+                           full.names = TRUE))
+
+# read in the global extent raster
+global <- raster("Z:/users/joshua/Snakebite/raster_mask/CoastGlobal_5k.tif")
+
+# loop through, open raster, and stack
+for (i in 1:length(list)){
+  
+  # get path to each spp.
+  path <- as.character(list[i])
+  
+  # read in raster
+  snake_rast <- raster(path)
+  
+  # extend the extent to match the global raster
+  snake_ext <- extend(snake_rast, global, value = NA)
+  
+  # stack the rasters
+  if(i == 1){
+  
+    snake_stack <- snake_ext
+  
+    } else {
+    
+    snake_stack <- stack(snake_stack, snake_ext)  
+    
+  }
+  
+}
+
+# get sum of values for each pixel in stack (basically combine all rasters
+# and generate a value of number of snake spp. per pixel)
+richness <- sum(snake_stack, na.rm = TRUE)
+
+# check it's worked
+plot(richness)
+
+# write out combined output
+writeRaster(richness,
+            file = 'Z:/users/joshua/Snakebite/output/species_richness',
+            format = 'GTiff',
+            overwrite = TRUE)
+
