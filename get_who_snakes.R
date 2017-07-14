39cd688c00aa52f3e6a5337183fd17da8255e094
Joshua Longbottom <josh.longbottom@hotmail.com>
2017-07-14 09:40:03 +0100

    Housekeeping; merged with 'convert_maps_to_png.R'
    Housekeeping; merged with 'convert_maps_to_png.R', convert to png has
    now been removed from the repo.

--- get_who_snakes.R
+++ get_who_snakes.R
@@ -2,7 +2,7 @@
 # clear workspace
 rm(list = ls())
 
-require(utils)
+pacman::p_load(utils, pdftools)
 
 # get snakes (list of snakes species on WHO website, as csv)
 # list was compiled from copying source data
@@ -33,5 +33,36 @@ for(i in 1:length(species_list)){
   download.file(spp_url,
                 outpath,
                 mode = "wb")
+  
+}
+
+# list the downloaded pdf files
+list <- as.list(list.files("Z:/users/joshua/Snakebite/WHO EOR PDFs/", pattern = '.pdf$'))
+
+# loop through and convert these pdf files into png
+for(i in 1:length(list)){
+  
+  # get file name
+  file_name <- unlist(list[i])
+  
+  # get species name
+  spp <- gsub('.pdf$', '', file_name)
+  
+  # gen path to pdf
+  paths <- paste("Z:/users/joshua/Snakebite/WHO EOR PDFs/",
+                 file_name,
+                 sep = "")
+  
+  # open file and render to png
+  bitmap <- pdf_render_page(paths, page = 1, dpi = 300)
+  
+  # generate outpath
+  outpath <- paste("Z:/users/joshua/Snakebite/WHO EOR PNG/",
+                   spp,
+                   ".png",
+                   sep = "")
+  
+  # save bitmap image
+  png::writePNG(bitmap, outpath)
   
 }
