dc28a53abf6fc94b99be60d622e5e248dad56039
Joshua Longbottom <josh.longbottom@hotmail.com>
2017-07-14 09:36:37 +0100

    Updates to allow execution in parallel and split by category
    Updated to run in parallel on the cluster.
    Script now generates a raster for each medical class (separate c1, c2,
    and a combined c1 & c2).

--- convert_shp_to_raster.R
+++ convert_shp_to_raster.R
@@ -2,56 +2,115 @@
 rm(list = ls())
 
 # load required libraries
-require(raster)
+pacman::p_load(raster, foreach, doMC)
 
-# list all extracted shapefiles
-list <- as.list(list.files("Z:/users/joshua/Snakebite/WHO EOR shapefiles/", 
-                           pattern = '*shp$'))
+# list all files in the 'modified_ranges' folder
+# this folder should contain the 'updated range' for a species; if no range modification has occurred, 
+# this folder will contain the original digitised EOR shapefile
+shape_list <- list.files('output/modified_ranges',
+                         pattern = ".shp$",
+                         full.names = TRUE)
+
+# read in a 5 km x 5 km raster to use as a template to rasterize by
+raster_mask <- raster('data/raw/raster/land_sea/CoastGlobal_5k.tif')
+
+# initialize the cluster
+registerDoMC(50)
 
 # loop through and convert shape files 
-for(i in 1:length(list)){
+raster_range <- foreach(i = 1:length(shape_list)) %dopar% {
   
   # get file name
-  file_name <- unlist(list[i])
+  file_name <- shape_list[i]
   
   # get snake species name
-  spp <- gsub('.shp$', '', file_name)
-  
-  # gen path to shapefile
-  paths <- paste("Z:/users/joshua/Snakebite/WHO EOR shapefiles/",
-                 file_name,
-                 sep = "")
-  
-  # read in a 5 km x 5 km raster to use as a template to rasterize by
-  raster_mask <- raster("Z:/users/joshua/Snakebite/raster_mask/CoastGlobal_5k.tif")
+  spp <- gsub('/home/josh/snakebite/output/modified_ranges/', '', file_name)
+  spp <- gsub('.shp$', '', spp)
   
   # inform progress
-  message(paste('rasterizing file', i, 'of', length(list), sep = " "))
+  message(paste('rasterizing file', i, 'of', length(shape_list), sep = " "))
   
   # open shapefile
-  snake_shape <- shapefile(paths)
+  snake_shape <- shapefile(file_name)
   
   # get extents of shapefile
   snake_extent <- extent(snake_shape)
   
   # crop raster mask extent by snake_extent
-  raster_mask <- crop(raster_mask, snake_extent)
+  raster_crop <- crop(raster_mask, snake_extent)
   
-  # convert snake shapefile to raster
-  snake_raster <- rasterize(snake_shape, raster_mask)
+  # create up to three different rasters based on the medical classification of each species
+  # generate a med class one shapefile
+  class_one_shp <- snake_shape[snake_shape$Med_Class == 1, ]
+  
+  # generate a med class two shapefile (which is c2 and c3 in instances where c3 exists)
+  class_two_shp <- snake_shape[snake_shape$Med_Class >= 2, ]
+  
+  # convert snake shapefiles to raster
+  # convert class one
+  if(length(class_one_shp) > 0){
+    
+    # rasterize to extent 
+    class_one_raster <- rasterize(class_one_shp, raster_crop)
+    
+    # set all raster cells = 1
+    class_one_raster[!is.na(class_one_raster)] <- 1
+    
+    # generate outpath
+    class_one_outpath <- paste('output/species_rasters/cat_1/',
+                               spp,
+                               sep = "")
+    
+    # save raster
+    writeRaster(class_one_raster, 
+                file = class_one_outpath,
+                format = "GTiff",
+                overwrite = TRUE)
+    
+  }
+  
+  # convert class two
+  if(length(class_two_shp) > 0){
+    
+    # rasterize to extent 
+    class_two_raster <- rasterize(class_two_shp, raster_crop)
+    
+    # set all raster cells = 1
+    class_two_raster[!is.na(class_two_raster)] <- 1
+    
+    # generate outpath
+    class_two_outpath <- paste('output/species_rasters/cat_2/',
+                               spp,
+                               sep = "")
+    
+    # save raster
+    writeRaster(class_two_raster, 
+                file = class_two_outpath,
+                format = "GTiff",
+                overwrite = TRUE)
+    
+  }
+  
+  # generate a combined (class 1 and class 2) raster
+  # rasterize to extent
+  combined_raster <- rasterize(snake_shape, raster_crop)
   
   # set all raster cells = 1
-  snake_raster[!is.na(snake_raster)] <- 1
+  combined_raster[!is.na(combined_raster)] <- 1
   
   # generate outpath
-  outpath <- paste("Z:/users/joshua/Snakebite/WHO EOR rasters/",
+  outpath <- paste('output/species_rasters/combined_cat/',
                    spp,
                    sep = "")
   
   # save raster
-  writeRaster(snake_raster, 
+  writeRaster(combined_raster, 
               file = outpath,
               format = "GTiff",
               overwrite = TRUE)
+  
+  
+  complete <- 'yes'
+  return(complete)
   
 }
