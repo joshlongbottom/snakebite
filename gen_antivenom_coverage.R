5813e1cb578337b805e7a4b7f762ebb22fb796be
Joshua Longbottom <josh.longbottom@hotmail.com>
2017-07-25 17:28:37 +0100

    Optimised to run on the server

--- gen_antivenom_coverage.R
+++ gen_antivenom_coverage.R
@@ -3,29 +3,36 @@
 rm(list = ls())
 
 # load required packages
-require(raster)
+pacman::p_load(raster, foreach, doMC)
 
 # read in antivenom csv
-venom <- read.csv("Z:/users/joshua/Snakebite/snakebite/antivenom.csv",
+venom <- read.csv('data/raw/antivenom.csv',
                   stringsAsFactors = FALSE, 
                   na.strings = c("NA","NaN", "", " "))
 
 # read in associated raster info dataset
-snakes <- read.csv("Z:/users/joshua/Snakebite/snakebite/snake_list.csv",
-                   stringsAsFactors = FALSE,
-                   na.strings = c("NA","NaN", "", " "))
+snakes <- read.csv('data/raw/snake_list_cluster.csv',
+                   stringsAsFactors = FALSE)
 
 # read in the global extent raster
-global <- raster("Z:/users/joshua/Snakebite/raster_mask/CoastGlobal_5k.tif")
+global <- raster('data/raw/raster/land_sea/CoastGlobal_5k.tif')
+
+# specify out directory
+out_dir <- 'output/antivenom_coverage'
 
 # get info on the snake species covered by each antivenom
 antivenom_products <- as.list(names(venom))
 
+# initialize the cluster (50 cores)
+registerDoMC(50)
+
+# time process
+message(paste('Generating antivenom coverage surfaces', Sys.time(), sep = ' '))
+
 # loop through each antivenom product, and generate a raster showing the effective
 # coverage of each product, based on the distribution of the snakes spp. that it is
 # effective for
-
-for (i in 1:length(antivenom_products)){
+antivenom_surface <- foreach(i = 1:length(antivenom_products)) %dopar% {
   
   # get antivenom product index
   # 1. get product name
@@ -71,15 +78,19 @@ for (i in 1:length(antivenom_products)){
   antivenom_coverage <- sum(snake_stack, na.rm = TRUE)
 
   # generate antivenom outpath
-  outpath <- paste('Z:/users/joshua/Snakebite/output/antivenom_coverage/', 
-                   product_name,
-                   sep = '')
+  outpath <- paste0(out_dir, 
+                    product_name)
 
   # 6. write out combined output
   writeRaster(antivenom_coverage,
               file = outpath,
               format = 'GTiff',
               overwrite = TRUE)
+  
+  complete <- paste0(product_name, ' complete')
+  
+  return(complete)
 
 }
 
+message(paste('Completed generating antivenom coverage surfaces', Sys.time(), sep = ' '))
