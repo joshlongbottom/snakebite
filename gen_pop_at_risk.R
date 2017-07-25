d9a7fdd7118b9c82fcb3575eb2a2652856794e8c
Joshua Longbottom <josh.longbottom@hotmail.com>
2017-07-25 16:37:35 +0100

    Updated to generate zonal statistics
    Updates include:
    - Utilizing an aggregated global pop surface (now at 5km vs 1km)
    - Generate one PAR surface for '1 or more species per cell' risk
    - Generate one PAR surface for '1 or more therapy naive species per
    cell'
    - Convert these PAR estimates to national estimates, using zonal
    
    -

--- gen_pop_at_risk.R
+++ gen_pop_at_risk.R
@@ -3,116 +3,79 @@
 rm(list = ls())
 
 # load required packages
-pacman::p_load(raster, foreach, doMC)
+pacman::p_load(raster)
 
 # load species richness surface
-species_richness <- raster('Z:/users/joshua/Snakebite/output/species_richness.tif')
+species_richness <- raster('Z:/users/joshua/Snakebite/output/species_richness/non-modified_eor_combined_categories_2017-07-25.tif')
 
 # load antivenom naive surface
 antivenom <- raster('Z:/users/joshua/Snakebite/output/antivenom_coverage/No_specific_antivenom.tif')
 
 # load population density surface
-pop_dens <- raster('Z:/mastergrids/Other_Global_Covariates/Population/Worldpop_GPWv4_Hybrid_201601/Global_Pop_1km_Adj_MGMatched_2015_Hybrid.tif')
+pop_dens <- raster('Z:/users/joshua/Snakebite/rasters/population/Worldpop_GPWv4_Hybrid_201601_Global_Pop_5km_Adj_MGMatched_2015_Hybrid.tif')
 
 # load admin 0 raster
-admin_0 <- brick('Z:/users/joshua/Cx.tritaeniorhynchus/Culex Tritaeniorhynchus/data/clean/raster/polygon_rasters/admin0_raster.flt')
+admin_0 <- raster('Z:/users/joshua/Cx.tritaeniorhynchus/Culex Tritaeniorhynchus/data/clean/raster/polygon_rasters/admin0_raster.flt')
 
-# rasterize spp richness raster to be at the same spatial res as the 1km pop
-species_richness <- disaggregate(species_richness, fact = c(5,5))
+# extend to the same extent as species richness surface
+admin_0 <- extend(admin_0, species_richness, value = NA)
 
-# rasterize antivenom surface at the same spatial res as the 1km pop
-antivenom <- disaggregate(antivenom, fact = c(5,5))
-
-# extend world pop to the same extent as the spp richness raster
-pop_dens <- extend(pop_dens, species_richness, value = NA)
-
-# get a count of the maximum number of species in the richness surface
-spp_vals <- values(species_richness)
-n_spp <- max(spp_vals)
-
-# initialize the cluster (50 cores)
-registerDoMC(50)
-
-### generate population living in areas suitable for x or more med spp
-par_stack <- foreach(i = 1:n_spp) %dopar% {
+### generate population living in areas suitable for 1 or more med spp
+# convert species richness surface into a binary surface (`1` = presence of 1 or more venomous
+# snake species, `NoData` = absence)
+species_presence <- species_richness
+species_presence[species_presence < 1 ] <- 0
+species_presence[species_presence  >= 1] <- 1
   
-  # convert species richness surface into a binary surface (`1` = presence of `i` quantity of venomous
-  # snake species, `NoData` = absence)
-  species_presence <- species_richness
-  species_presence[species_presence < i ] <- 0
-  species_presence[species_presence  >= i] <- 1
-  
-  # multiply the population by the binary presence/absence surface
-  presence_par <- overlay(pop_dens, species_presence, fun = function(pop_dens, species_presence){
+# multiply the population by the binary presence/absence surface
+presence_par <- overlay(pop_dens, species_presence, fun = function(pop_dens, species_presence){
                                                                     (pop_dens*species_presence)})
-  
-  return(presence_par)
-  
-}
 
-names(par_stack) <- paste0("par_exposure_", 1:n_spp)
-list2env(par_stack, envir = .GlobalEnv)
+# convert this to a national estimate using zonal()
+national_par <- zonal(presence_par, admin_0, fun = 'sum', na.rm = TRUE)
+national_par <- as.data.frame(national_par)
 
-# get a count of the maximum number of species in the antivenom naive surface
-naive_spp <- values(antivenom)
-n_naive <- max(naive_spp)
+# match this to get location names
+# read in admin 0 shapefile dbf
+countries <- foreign::read.dbf("Z:/users/joshua/admin2013/admin2013_0.dbf")
 
-# initialize the cluster (50 cores)
-registerDoMC(50)
+# create an matching index
+match_idx <- match(national_par$zone, countries$GAUL_CODE)
 
-### generate population living in areas suitable for x or more med spp
-antiv_par_stack <- foreach(i = 1:n_naive) %dopar% {
-  
-  # convert antivenom naive surface into a binary surface (`1` = presence of `i` quantity of therapy
-  # naive snake species, `NoData` = absence)
-  antiv_n_naive <- antivenom
-  antiv_n_naive[antiv_n_naive < i ] <- 0
-  antiv_n_naive[antiv_n_naive  >= i] <- 1
-  
-  # multiply the population by the binary presence/absence antivenom surface
-  presence_par <- overlay(pop_dens, species_presence, fun = function(pop_dens, species_presence){
-                                                                    (pop_dens*species_presence)})
-  
-  return(presence_par)
-  
-}
+# append iso and country name
+national_par$iso <- countries$COUNTRY_ID[match_idx]
+national_par$name <- countries$name[match_idx]
 
-names(antiv_par_stack) <- paste0("antivenom_naive_", 1:n_spp)
-list2env(antiv_par_stack, envir = .GlobalEnv)
+# write out par of exposure to 1 or more snake species
+write.csv(national_par,
+          'Z:/users/joshua/Snakebite/output/population_at_risk/exposure_to_one_or_more_spp.csv',
+          row.names = FALSE)
 
-# generate global PAR totals for surface
-# loop through each PAR raster, and get a sum of the pixel values to generate a global
-# PAR estimate
-par_totals <- for(i in 1:n_spp) {
+### generate population living in areas suitable for 1 or more medically important spp for which
+### no effective therapy exists
+# convert antivenom naive surface into a binary surface (`1` = presence of 1 or more therapy
+# naive snake species, `NoData` = absence)
+antiv_n_naive <- antivenom
+antiv_n_naive[antiv_n_naive < 1 ] <- 0
+antiv_n_naive[antiv_n_naive  >= 1] <- 1
   
-  # get surface 
-  obj_name <- paste0("par_exposure_", i)
-  
-  # get cell values across surface
-  par_exp_vals <- values(obj_name)
+# multiply the population by the binary presence/absence antivenom surface
+naive_par <- overlay(pop_dens, antiv_n_naive, fun = function(pop_dens, antiv_n_naive){
+                                                            (pop_dens*antiv_n_naive)})
 
-  # sum cell values across surface
-  par_totals <- sum(par_exp_vals)
-  
-  return(par_totals)
-  
-}
+# convert this to a national estimate using zonal()
+national_naive_par <- zonal(naive_par, admin_0, fun = 'sum', na.rm = TRUE)
+national_naive_par <- as.data.frame(national_naive_par)
 
-# generate global PAR totals for antivenom naive
-anti_par_totals <- for(i in 1:n_naive) {
-  
-  # get surface 
-  obj_name <- paste0("antivenom_naive_", i)
-  
-  # get cell values across surface
-  par_naive_vals <- values(obj_name)
-  
-  # sum cell values across surface
-  par_totals <- sum(par_naive_vals)
-  
-  return(par_totals)
-  
-}
+# create an matching index
+rm(match_idx)
+match_idx <- match(national_naive_par$zone, countries$GAUL_CODE)
 
-# generate national estimates
+# append iso and country name
+national_naive_par$iso <- countries$COUNTRY_ID[match_idx]
+national_naive_par$name <- countries$name[match_idx]
 
+# write out par of exposure to 1 or more therapy naive snake species
+write.csv(national_naive_par,
+          'Z:/users/joshua/Snakebite/output/population_at_risk/exposure_to_one_or_more_therapy_naive_spp.csv',
+          row.names = FALSE)
