a9bd71e6171d4c8ebb16279fc91c797e791bcedb
Joshua Longbottom <josh.longbottom@hotmail.com>
2017-07-26 13:44:41 +0100

    Modified script to enhance running in parallel, and split by medical class.
    Modified script to enhance running in parallel, and split by medical
    class.
    Script now generates one surface per medical class, for each antivenom.
    Currently only processed for 'no specific antivenom', however, loop is
    established to allow generation of surfaces for all available
    antivenoms.
    Script now also excludes snakes which we only have at genus level
    (Acanthophis spp., Bothrops spp., Crotalus spp. and Micrurus spp.).

--- gen_antivenom_coverage.R
+++ gen_antivenom_coverage.R
@@ -17,22 +17,34 @@ snakes <- read.csv('data/raw/snake_list_cluster.csv',
 # read in the global extent raster
 global <- raster('data/raw/raster/land_sea/CoastGlobal_5k.tif')
 
-# specify out directory
-out_dir <- 'output/antivenom_coverage'
+# specify directories:
+# out directory
+out_dir <- 'output/antivenom_coverage/'
+# cat 1 rasters
+cat_1_ras <- 'output/species_rasters/cat_1/'
+# cat 2 rasters
+cat_2_ras <- 'output/species_rasters/cat_2/'
+# combined rasters
+combined_ras <- 'output/species_rasters/combined_cat/'
+
+# define category ids
+c1_id <- c('1', '1&2')
+c2_id <- c('2', '1&2')
 
 # get info on the snake species covered by each antivenom
 antivenom_products <- as.list(names(venom))
 
-# initialize the cluster (50 cores)
-registerDoMC(50)
-
-# time process
-message(paste('Generating antivenom coverage surfaces', Sys.time(), sep = ' '))
+# drop snakes in the list for which we only have species level data
+snakes_to_drop <- c('Acanthophis_spp', 'Bothrops_spp', 'Crotalus_spp', 'Micrurus_spp')
+snakes <- snakes[!snakes$split_spp %in% snakes_to_drop, ]
 
 # loop through each antivenom product, and generate a raster showing the effective
 # coverage of each product, based on the distribution of the snakes spp. that it is
 # effective for
-antivenom_surface <- foreach(i = 1:length(antivenom_products)) %dopar% {
+for(i in 1:length(antivenom_products)){
+  
+  # time process
+  message(paste('Generating antivenom coverage surface', i, 'of', length(antivenom_products), Sys.time(), sep = ' '))
   
   # get antivenom product index
   # 1. get product name
@@ -44,53 +56,128 @@ antivenom_surface <- foreach(i = 1:length(antivenom_products)) %dopar% {
   # 3. grab paths for spp. rasters
   snake_idx <- which(snakes$id %in% effective_snakes)
   snake_sub <- snakes[snake_idx, ]
-  list <- unique(snake_sub$raster_path, na.rm = TRUE)
-  list <- list[!is.na(list)]
+  # remove ones without a raster
+  snake_sub <- snake_sub[!is.na(snake_sub$raster_path), ]
   
-  # 4. loop through, open raster, and stack
-  for (i in 1:length(list)){
+  # split by category
+  cat_1_sub <- snake_sub[snake_sub$processing_cat %in% c1_id, ]
+  cat_2_sub <- snake_sub[snake_sub$processing_cat %in% c2_id, ]
   
-  # get path to each spp.
-  path <- as.character(list[i])
+  # list individual species
+  c1_list <- unique(cat_1_sub$split_spp, na.rm = TRUE)
+  c2_list <- unique(cat_2_sub$split_spp, na.rm = TRUE)
+  combined_list <- unique(snake_sub$split_spp, na.rm = TRUE)
   
-  # read in raster
-  snake_rast <- raster(path)
+  # initialize the cluster (50 cores)
+  registerDoMC(50)
   
-  # extend the extent to match the global raster
-  snake_ext <- extend(snake_rast, global, value = NA)
-  
-  # stack the rasters
-  if(i == 1){
+  # 4. for each medical class, loop through species, open rasters and stack
+  if(length(c1_list) > 0){
     
-    snake_stack <- snake_ext
+    c1_antivenom_surface <- foreach(i = 1:length(c1_list)) %dopar% {
+      
+      # get path to each spp.
+      spp <- as.character(c1_list[i])
+      path <- paste0(cat_1_ras, spp, '.tif')
+      
+      # read in raster
+      snake_rast <- raster(path)
+      
+      # extend the extent to match the global raster
+      snake_ext <- extend(snake_rast, global, value = NA)
+      
+      return(snake_ext)
+      
+    }
     
-  } else {
-    
-    snake_stack <- stack(snake_stack, snake_ext)  
+    # stack the rasters
+    c1_stack <- stack(c1_antivenom_surface)
     
   }
   
+  if(length(c2_list) > 0){
+    
+    c2_antivenom_surface <- foreach(i = 1:length(c2_list)) %dopar% {
+      
+      # get path to each spp.
+      spp <- as.character(c2_list[i])
+      path <- paste0(cat_2_ras, spp, '.tif')
+      
+      # read in raster
+      snake_rast <- raster(path)
+      
+      # extend the extent to match the global raster
+      snake_ext <- extend(snake_rast, global, value = NA)
+      
+      return(snake_ext)
+      
+    }
+    
+    # stack the rasters
+    c2_stack <- stack(c2_antivenom_surface)
+    
   }
-
-  # 5. get sum of values for each pixel in stack (basically combine all rasters
-  # and generate a value of number of snake spp. per pixel covered by each 
-  # effective antivenom)
-  antivenom_coverage <- sum(snake_stack, na.rm = TRUE)
-
-  # generate antivenom outpath
-  outpath <- paste0(out_dir, 
-                    product_name)
-
-  # 6. write out combined output
-  writeRaster(antivenom_coverage,
-              file = outpath,
-              format = 'GTiff',
-              overwrite = TRUE)
   
-  complete <- paste0(product_name, ' complete')
+  if(length(combined_list) > 0){
+    
+    comb_antiv_surface <- foreach(i = 1:length(combined_list)) %dopar% {
+      
+      # get path to each spp.
+      spp <- as.character(combined_list[i])
+      path <- paste0(combined_ras, spp, '.tif')
+      
+      # read in raster
+      snake_rast <- raster(path)
+      
+      # extend the extent to match the global raster
+      snake_ext <- extend(snake_rast, global, value = NA)
+      
+      return(snake_ext)
+      
+    }
+    
+    # stack the rasters
+    combined_stack <- stack(comb_antiv_surface)
+    
+  }
   
-  return(complete)
-
+  # 5. get sum of values for each pixel in stack (basically combine all rasters and 
+  # generate a value of number of snake spp. per pixel covered by each effective antivenom)
+  loop_list <- as.list(c('c1_stack', 'c2_stack', 'combined_stack'))
+  
+  # do this in parallel (if possible - may be too computationally heavy)
+  gen_surface <- foreach(i = 1:length(loop_list)) %dopar% {
+    
+    # check that the stack exists
+    if(length(loop_list[[i]] > 0)) {
+      
+    antivenom_coverage <- sum(loop_list[[i]], na.rm = TRUE)
+    
+    # generate antivenom outpath
+    outpath <- paste0(out_dir, 
+                      product_name,
+                      '_',
+                      loop_list[[i]])
+    
+    # 6. write out combined output
+    writeRaster(antivenom_coverage,
+                file = outpath,
+                format = 'GTiff',
+                overwrite = TRUE)
+    
+    complete <- paste(product_name, loop_list[[i]], 'complete = TRUE', sep = " ")
+    
+    return(complete)
+    
+    } else{
+    
+    complete <- paste(product_name, loop_list[[i]], 'complete = FALSE', sep = " ")  
+  
+    }
+  
+    }  
+  
+  message(paste('Completed generating antivenom coverage surface', i, 'of', length(antivenom_products), Sys.time(), sep = ' '))
+  
 }
 
-message(paste('Completed generating antivenom coverage surfaces', Sys.time(), sep = ' '))
